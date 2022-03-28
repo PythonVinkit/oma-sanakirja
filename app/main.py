bf631d9024d5fb639f9c6c83924833a9a50ab287
Altesmi <optikkanen@gmail.com>
2022-03-28 21:05:58 +0300

    index ja layout

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from typing import List
-from fastapi import FastAPI, APIRouter
+from pathlib import Path
+from fastapi import FastAPI, APIRouter, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from app.demo_data.users import demo_users
 
 from app.models.schemas import User
@@ -7,10 +11,13 @@ from app.models.schemas import User
 app = FastAPI(title="Oma sanakrija", openapi_url="/openapi.json")
 
 api_router = APIRouter()
+BASE_PATH = Path(__file__).resolve().parent
+TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
-@api_router.get("/", status_code=200)
-def root() -> dict:
-    return {"msg": "moi"}
+app.mount("/static", StaticFiles(directory=str(BASE_PATH / "static")), name="static")
+@api_router.get("/", status_code=200, response_class=HTMLResponse)
+def root(request: Request) -> dict:
+    return TEMPLATES.TemplateResponse("index.html", {"request": request})
 
 @api_router.get("/users", response_model=List[User])
 def users() -> list:
